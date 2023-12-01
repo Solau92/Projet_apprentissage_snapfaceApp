34484473f3baeac34eed9687e33d8be9e4f3a436
Sophie LAURENT <sophielaurent92@gmail.com>
2023-12-01 20:23:52 +0100

    New component

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit(): void {
+    this.title = 'myTitle';
+    this.description = 'myDescription';
+    this.createdDate = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'assets/images/snap1.jpg';
+  }
 }
