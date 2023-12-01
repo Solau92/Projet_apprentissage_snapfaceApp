a78dec65f2f5fd81a0d34535e9c47819085269be
Sophie LAURENT <sophielaurent92@gmail.com>
2023-12-01 16:59:22 +0100

    New version --no-standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent
   ],
   imports: [
     BrowserModule,
