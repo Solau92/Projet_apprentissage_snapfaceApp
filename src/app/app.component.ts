a791a9d875b9b49511931547f1bd84a300704eb5
Sophie LAURENT <sophielaurent92@gmail.com>
2023-12-04 10:55:25 +0100

    *ngFor, [ngStyle], [ngClass]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,35 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
 
-    this.mySnap =  {
+    this.faceSnaps = [
+    {
       title:'myTitle',
       description: 'myDescription',
       createdDate: new Date(),
-      snaps: 0,
+      snaps: 255,
       imageUrl: 'assets/images/snap1.jpg',
       location:'Courbevoie'
-    };
- 
-    this.myOtherSnap = {
+    }, 
+    {
       title:'myOtherTitle',
       description: 'myOtherDescription',
       createdDate: new Date(),
-      snaps: 0,
-      imageUrl: 'assets/images/snap2.jpg'
-    }; 
-    
-    this.myLastSnap = {
+      snaps: 120,
+      imageUrl: 'assets/images/snap2.jpg',
+      location:'Paris'
+    },
+    {
       title:'myLastTitle',
       description: 'myLastDescription',
       createdDate: new Date(),
       snaps: 0,
       imageUrl: 'assets/images/snap3.jpg' 
-    };  
+    }];  
 
   }
 }
