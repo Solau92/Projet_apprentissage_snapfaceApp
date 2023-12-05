96d64d47159c06ca08362407b6982f429c09b244
Sophie LAURENT <sophielaurent92@gmail.com>
2023-12-05 11:09:23 +0100

    Locale, pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +18,8 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    { provide : LOCALE_ID, useValue: 'fr-FR'}
   ],
   bootstrap: [AppComponent]
 })
